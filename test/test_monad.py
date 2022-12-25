46e390225b0d51d6a4efe21af502249f99cef279
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-12-25 20:33:36 -0300

    Implemented test_monad

--- test/test_monad.py
+++ test/test_monad.py
@@ -6,13 +6,22 @@ class TestResult(TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.justs = [Optional(5), Optional(0), Optional(""), Optional("None")]
+        self.monad = Optional(5)
 
     def test_bind(self):
-        ...
+        self.assertEqual(Optional(6),
+                         self.monad.bind(lambda x: Optional(x + 1)))
 
     def test_unwrap(self):
-        ...
+        self.assertEqual(5, self.monad.unwrap())
+
+    def test___rshift__(self):
+        self.assertEqual(Optional(6),
+                         self.monad >> (lambda x: Optional(x + 1)))
 
     def test_functionConvert(self):
-        ...
+        @Optional.functionConvert
+        def addOne(x):
+            return x + 1
+
+        self.assertEqual(Optional(6), self.monad >> addOne)
