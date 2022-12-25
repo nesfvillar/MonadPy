5480b11d3e811e9d7fd81c691ee663618b165b8c
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-12-25 20:33:09 -0300

    Added syntax for binding functions with >> op

--- monads/monad.py
+++ monads/monad.py
@@ -2,9 +2,15 @@ class Monad:
     def bind(self, f):
         return f(self._value)
 
+    def __rshift__(self, func):
+        return self.bind(func)
+
     def unwrap(self):
         return self._value
 
     @classmethod
     def functionConvert(cls, func):
         return lambda *args, **kwargs: cls(func(*args, **kwargs))
+
+    def __eq__(self, __monad):
+        return self.unwrap() == __monad.unwrap()
