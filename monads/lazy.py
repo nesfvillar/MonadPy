9e52a5f41ca5e6452d6fd79a27c3dae6ae55bcfa
Nestor Fabian Villar <nesfvillar@gmail.com>
2023-09-24 19:53:03 -0300

    Fixed not iterable

--- monads/lazy.py
+++ monads/lazy.py
@@ -17,7 +17,7 @@ class Lazy(Monad[A]):
     _funcs: Iterable[Callable[[Any], Any | A]] = field(default_factory=list)
 
     def fmap(self, func: Callable[[A], B]) -> Lazy[B]:
-        return Lazy(self._value, chain(self._funcs, (func)))
+        return Lazy(self._value, chain(self._funcs, [func]))
 
     def unwrap(self) -> A:
         value = self._value
