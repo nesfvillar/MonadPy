7f845535776e0e4768d39211baf68a4f6f8905e8
Nestor Fabian Villar <nesfvillar@gmail.com>
2023-09-24 15:58:22 -0300

    Use itertools chain to limit the creation of lists

--- monads/lazy.py
+++ monads/lazy.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 from .monad import Monad
 
+from collections.abc import Callable, Iterable
 from dataclasses import dataclass, field
-from collections.abc import Callable
+from itertools import chain
 from typing import Any, TypeVar
 
 A = TypeVar("A")
@@ -13,10 +14,10 @@ B = TypeVar("B")
 @dataclass(frozen=True)
 class Lazy(Monad[A]):
     _value: Any
-    _funcs: list[Callable[[Any], Any | A]] = field(default_factory=list)
+    _funcs: Iterable[Callable[[Any], Any | A]] = field(default_factory=list)
 
     def fmap(self, func: Callable[[A], B]) -> Lazy[B]:
-        return Lazy(self._value, self._funcs + [func])
+        return Lazy(self._value, chain(self._funcs, (func)))
 
     def unwrap(self) -> A:
         value = self._value
